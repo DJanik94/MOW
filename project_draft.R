4185fad23bd847fe5a9fc200f71d7404d80f6154
Dawid <dawidjanik1294@gmail.com>
2019-05-19 22:38:53 +0200

    Manual test of trees for feature selection treshold

--- project_draft.R
+++ project_draft.R
@@ -5,7 +5,7 @@ library("data.table")
 library(caret)
 library(randomForest)
 library('e1071')
-
+library(dplyr)
 # ---------------- Data read -------------
 setwd('~/Studia/SEM2/MOW/Projekt/MOW')
 
@@ -19,66 +19,69 @@ print(paste("Incomplete cases:", incomplete_count, sep = " "))
 #---- Build a random forest based on whole set for feature selection ---------------------------
 feature_selection_rf <- randomForest(formula = Attrition~., data = dat)
 print(summary(feature_selection_rf))
-forestSummary <- as.data.frame(varImp(feature_selection_rf))
+forestSummary <- varImp(feature_selection_rf)
 forestSummary <- data.frame(overall = forestSummary$Overall,
                   names   = rownames(forestSummary))
-forestSummary <- forestSummary[order(forestSummary$overall, decreasing = T), ]
-forestSummary
+forestSummarySorted <- forestSummary[order(forestSummary$overall, decreasing = T), ]
+rownames(forestSummarySorted) <- NULL
 varImpPlot(feature_selection_rf)
 
 #---- Test models based on different numbers of features (Initial values: 10, 15, 20, all attributes )
-feature_tresh1 <- 1:10
-feature_tresh2 <- 1:15
-feature_tresh3 <- 1:20
-feature_all <- 1:ncol(dat)
+#feature_tresh1 <- forestSummarySorted$names[1:10]
+#feature_tresh2 <- forestSummarySorted$names[1:15] 
+#feature_tresh3 <- forestSummarySorted$names[1:20]
+#feature_all <- forestSummarySorted$names
 
-#training_set_size = 1:round(0.8*nrow(dat)) 
+feature_tresh1 <- colnames(subset(dat, select = c(MonthlyIncome,Age,OverTime,DailyRate,TotalWorkingYears,MonthlyRate,JobRole,DistanceFromHome,HourlyRate,YearsAtCompany,PercentSalaryHike)))
+feature_tresh2 <- c(feature_tresh1, colnames(subset(dat, select = c(NumCompaniesWorked,EducationField,YearsWithCurrManager,EnvironmentSatisfaction,StockOptionLevel))))
+
+
 training_set_size <- round(0.8*nrow(dat))
 train_index  <- sample(seq_len(nrow(dat)), size = training_set_size)
-treshholds <- list(feature_tresh1, feature_tresh2, feature_tresh3, feature_all)
-
+#treshholds <- list(feature_tresh1)#, feature_tresh2, feature_tresh3, feature_all)
+#treshholds <- list(feature_all)
 
 #Test results for differrent feature set sizes
-for (treshold in treshholds){
-  substet_t <- subset(dat, select = treshold)
-  training_set <- substet_t[train_index, ]
-  validation_set <- substet_t[-train_index, ]
-  rf_for_set_size <- randomForest(formula = Attrition~., data = substet_t)
-  pred <- predict(rf_for_set_size, newdata = validation_set, type = 'response')
-  conf_matrix <- caret::confusionMatrix(pred, validation_set$Attrition)
-  print(conf_matrix)
-}
+#for (treshold in treshholds){
+# Test for 10 features
+  #print(treshold)
+  substet_t1 <- subset(dat, select = c(MonthlyIncome,Age,OverTime,DailyRate,TotalWorkingYears,MonthlyRate,JobRole,DistanceFromHome,HourlyRate,YearsAtCompany,PercentSalaryHike, Attrition))
+  training_set1 <- substet_t1[train_index, ]
+  validation_set1 <- substet_t1[-train_index, ]
+  rf_for_set_size1 <- randomForest(formula = Attrition~., data = training_set1)
+  pred1 <- predict(rf_for_set_size1, newdata = validation_set1, type = 'response')
+  conf_matrix1 <- caret::confusionMatrix(pred1, validation_set1$Attrition)
+  print(conf_matrix1)
+#}
+  
+  substet_t2 <- subset(dat, select = c(MonthlyIncome,Age,OverTime,DailyRate,TotalWorkingYears,MonthlyRate,JobRole,DistanceFromHome,HourlyRate,YearsAtCompany,PercentSalaryHike, Attrition,NumCompaniesWorked,EducationField,YearsWithCurrManager,EnvironmentSatisfaction,StockOptionLevel))
+  training_set2 <- substet_t2[train_index, ]
+  validation_set2 <- substet_t2[-train_index, ]
+  rf_for_set_size2 <- randomForest(formula = Attrition~., data = training_set2)
+  pred2 <- predict(rf_for_set_size2, newdata = validation_set2, type = 'response')
+  conf_matrix2 <- caret::confusionMatrix(pred2, validation_set2$Attrition)
+  print(conf_matrix2)
+  
+  
+  substet_t3 <- subset(dat, select = c(MonthlyIncome,Age,OverTime,DailyRate,TotalWorkingYears,MonthlyRate,JobRole,DistanceFromHome,HourlyRate,YearsAtCompany,PercentSalaryHike, Attrition,NumCompaniesWorked,EducationField,YearsWithCurrManager,EnvironmentSatisfaction,StockOptionLevel,JobSatisfaction,TrainingTimesLastYear,WorkLifeBalance,YearsInCurrentRole,YearsSinceLastPromotion))
+  training_set3 <- substet_t3[train_index, ]
+  validation_set3 <- substet_t3[-train_index, ]
+  rf_for_set_size3 <- randomForest(formula = Attrition~., data = training_set3)
+  pred3 <- predict(rf_for_set_size3, newdata = validation_set3, type = 'response')
+  conf_matrix3 <- caret::confusionMatrix(pred3, validation_set3$Attrition)
+  print(conf_matrix3)
+  
+  
+  training_set4 <- dat[train_index, ]
+  validation_set4 <- dat[-train_index, ]
+  rf_for_set_size4 <- randomForest(formula = Attrition~., data = training_set4)
+  pred4 <- predict(rf_for_set_size4, newdata = validation_set4, type = 'response')
+  conf_matrix4 <- caret::confusionMatrix(pred4, validation_set4$Attrition)
+  print(conf_matrix4)
 
 
 
-#data_t2 <- subset(dat, select = feature_tresh2)
-#data_t3 <- subset(dat, select = feature_tresh3)
-
-#-----------
-#dat_shaped <- as.data.table(dataPreparation::shapeSet(dat, finalForm = 'numerical_matrix', verbose =TRUE))
-#dat_shaped <-subset(dat_shaped, select = -c(AttritionNo))
-#names(dat_shaped) <- str_replace_all(names(dat_shaped), c(" " = "_", "," = "", "&" = ""))
-#Split data into 3 sets
-
-#training_set_size = 1:round(0.6*nrow(dat)) 
-#validation_set_size = round(0.6*nrow(dat))+1:round(0.2*nrow(dat))
-#test_set_size = round(0.6*nrow(dat))+round(0.2*nrow(dat))+1: round(0.2*nrow(dat))
-
-#training_set_shaped <- dat_shaped[training_set_size, ]
-#validation_set_shaped <- dat_shaped[validation_set_size, ]
-#test_set_shaped <- dat_shaped[test_set_size,]
-
-#training_set <- dat[training_set_size, ]
-#validation_set <- dat[validation_set_size, ]
-#test_set <- dat[test_set_size,]
-
-
-
-# Model test (irrelevant)
-#pred <- predict(feature_selection_rf, newdata = validation_set_shaped, type = 'response')
-#caret::confusionMatrix(pred, validation_set_shaped$AttritionYes)
-
-# -----------------Calculate correlation --------------------
+  # -----------------Calculate correlation --------------------
 #M <- cor(dat_shaped)
 #head(round(M,2))
   
